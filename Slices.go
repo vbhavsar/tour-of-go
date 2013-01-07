44e20f6f89a7e8b3e0b32600ebe6829754530e4a
Vishal Bhavsar <vbhavsar@gmail.com>
2013-01-07 04:10:42 +0000

    Slices. Wont work without "tour"

---
+++ Slices.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+Implement Pic. It should return a slice of length dy, each element of which is a slice of dx 8-bit unsigned integers. When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
+http://tour.golang.org/#46
+*/
+
+import "tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	
+	rv := make([][]uint8, dx)
+	for x:=0; x<dx; x++{
+		rv[x] = make([]uint8, dy) 
+		for y:=0; y<dy; y++{
+			rv[x][y] = uint8((x+y)/3);
+		}
+	}
+	return rv;
+}
+
+func main() {
+	pic.Show(Pic)
+}
