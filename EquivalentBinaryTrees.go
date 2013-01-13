1072931c0ee666062267959c5c34dee200d3bb6a
vbhavsar <vbhavsar@gmail.com>
2013-01-13 13:40:01 -0500

    Fixed and working

--- EquivalentBinaryTrees.go
+++ EquivalentBinaryTrees.go
@@ -1,67 +1,45 @@
 package main
 
-// Still in progress
-
 import (
-  "tour/tree"
+	"tour/tree"
 	"fmt"
 	)
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
-	switch {
-	case t.Left != nil:
+	if t.Left != nil {
 		Walk(t.Left, ch)
-		ch <- t.Value
-		if (t.Right != nil){
-			Walk(t.Right, ch)
-		}
-	default:
-		ch <- t.Value
-		if (t.Right != nil){
-			Walk(t.Right, ch)
-		}
-		
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
 	}
 }
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
+
 	c1 := make(chan int)
 	c2 := make(chan int)
 	go Walk(t1, c1)
 	go Walk(t2, c2)
-
-	var x, y int
 	
-	select {
-	case c1 <- x:
-		c2 <- y
-		if x != y {
-			return false 
-		}
-	case c2<- y:
-		c1 <- x
-		if x != y {
-			return false
-		}
-	default:
-		return true
+	for (<-c1 != <-c2) {
+		return false
 	}
 	return true
 }
 
 func main() {
-/*
 	ch := make(chan int)
 	go Walk(tree.New(1), ch)
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-ch)
+
+	for i := 0; i<10; i++  {
+		fmt.Printf("%d ", <-ch)
 	}
-*/
+	fmt.Println()
 	fmt.Println(Same(tree.New(1), tree.New(1)))
 	fmt.Println(Same(tree.New(1), tree.New(2)))
-
 }
